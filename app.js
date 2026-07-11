9fbb457925e1c9427eba803a2cd89cc42f4ef3dc
Ritesh Shinde <shinderitesh217@gmail.com>
2026-07-12 01:30:22 +0530

    Implemented Basic Routing and request logging

--- app.js
+++ app.js
@@ -3,12 +3,32 @@ const url = require('url');
 
 
 const server = http.createServer((req,res)=>{
-	const path_name = req.url;
+	
 
-	if(req){
-		res.end("Caching proxy server");
+	const path_name = req.url;
+	const method = req.method;
+	const headers = req.headers;
+
+	console.log(path_name);
+	console.log(method);
+	console.log(headers);
+
+
+
+	if(path_name == '/'){
+		res.writeHead(200, {'Content-type' : 'text/plain'});
+		res.end("Welcome to the Proxy Project!");
 	}
 
+	else if(path_name == '/proxy'){
+		res.writeHead(200, {'Content-Type' : 'text/plain'});
+		res.end("This is going to be the proxy endpoint.");
+	}
+
+	else {
+		res.writeHead(404, {'Content-Type' : 'text/html'});
+		res.end('<h1>ERROR 404</h1>');
+	}
 
 });
 
